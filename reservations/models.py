5690e0b550d3ffdffeded7de5ce75f8d1858f1d7
Mustafa Akcanca <MAkcanca@users.noreply.github.com>
2021-06-09 11:38:41 +0300

    Added models

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Dog(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+
+class DogReservation(models.Model):
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
